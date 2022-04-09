d26d6c1eae7be0dd3e331d1ff7473e7cc9f84f1c
Juhyeon Kim <woonikim@naver.com>
2022-04-09 15:16:12 +0900

    [ADD] AppHeader 기본 작업

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    v-bind:class="{'app-header': true, shrink: shrink}"
+  >
+  Header
+  </div>
+</template>
+
+<script> 
+export default {
+  data() {
+    return {
+      shrink: false
+    }
+  },
+  mounted() {
+    // var lastScrollTop = 0;
+    window.addEventListener('scroll', () => {
+      var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTop > 50) {
+        this.shrink = true;
+      } else {
+        this.shrink = false;
+      }
+    });
+  }
+}
+</script>
+
+<style scoped>
+[class*='app-header'] {
+  display: block;
+  position: fixed;
+  text-align: center;
+  background-color: red;
+  top: 0;
+  height: 90px;
+  width: 100%;
+  transition: 0.3s;
+  transition-timing-function: cubic-bezier(0.295, 0.695, 0.290, 1.005);
+}
+
+.shrink {
+  height: 50px;
+  background-color: blue;
+  transition: 0.3s;
+}
+
+</style>
